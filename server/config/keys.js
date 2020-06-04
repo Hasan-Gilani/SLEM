358afe9f5fe03b1827ed3a3f6538e041b5913486
Hasan Gilani <hasan.05gilani@gmail.com>
2020-06-04 13:27:48 +0500

    Added mongodbAtlas Library database

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/Library",
-  mongoAtlas: "mongodb+srv://hasan:1234abcd@cluster0-qcel3.mongodb.net/test?retryWrites=true&w=majority",
+  mongoAtlas: "mongodb+srv://hasan:1234abcd@cluster0-qcel3.mongodb.net/Library?retryWrites=true&w=majority",
   secretOrKey: "my secret"
 };
