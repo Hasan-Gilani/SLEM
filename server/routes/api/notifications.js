d8fa4f0595a749f7bfd6e5cbf2f5d62e5123ad16
Hasan Gilani <hasan.05gilani@gmail.com>
2020-06-06 14:30:29 +0500

    added sports notification messages to notifications

--- server/routes/api/notifications.js
+++ server/routes/api/notifications.js
@@ -95,12 +95,67 @@ function getRegisterNotification(id, password, fname, lname) {
     return textBody.join("");
     }
 
+function getSpBorrow(sports, bdate, rdate){
+    let locBdate = bdate.split("GMT")[0], locRdate = rdate.split("GMT")[0];
+    let textBody = [`<h4><span style="color: #000000;"><em><strong>
+                     Dear Student,</strong></em></span></h4>
+                    <p style="text-align: left;"><span style="color: #000000;">&emsp;
+                    You have Loaned a sports good. Following are the details:</span></p>
+                    <ul>
+                    <li style="text-align: left;"><em>Good No:  </em>${sports.goodID}</li>
+                    <li style="text-align: left;"><em>Type: </em>${sports.goodType}</li>
+                    </ul>
+                    <p><em>Loan Date: </em>${locBdate}</p>
+                    <p><em>Return Date: </em>${locRdate}</p>
+                    <p><span style="color: #ff0000;">Note: </span><span style="color: #ff0000;">A fine of</span><em><span style="color: #ff0000;"> <strong>RS 100/- per each</strong></span><span style="color: #ff0000;"><strong> good</strong></span></em> <span style="color: #ff0000;">will have to be paid in case of a late-return.</span><em><span style="color: #ff0000;"><br /></span></em></p>
+                    <p style="font-size: 90%;"><em><span style="text-decoration: underline;">*This is a system generated E-mail. No response is required*</span></em></p>`
+    ];
+    return textBody.join("");
+}
 
+function getSpReturn(good, rdate){
+    let rTime = rdate.split("GMT")[0];
+    let textBody = [`<h4><span style="color: #000000;"><em><strong>
+                     Dear Student,</strong></em></span></h4>
+                    <p style="text-align: left;"><span style="color: #000000;">&emsp;
+                    You have returned a sports good . Following are the details:</span></p>
+                    <ul>
+                    <li style="text-align: left;"><em>Good No:  </em>${good.goodID}</li>
+                    <li style="text-align: left;"><em>Good Type: </em>${good.goodType}</li>
+                    </ul>
+                    <p><em>Return time: </em>${rTime}</p>
+                    <p><span style="color: #ff0000;">Note: </span><span style="color: #ff0000;">A fine of</span><em><span style="color: #ff0000;"> <strong>RS 100/- per each</strong></span><span style="color: #ff0000;"><strong> good</strong></span></em> <span style="color: #ff0000;">will have to be paid in case of a late-return.</span><em><span style="color: #ff0000;"><br /></span></em></p>
+                    <p style="font-size: 90%;"><em><span style="text-decoration: underline;">*This is a system generated E-mail. No response is required*</span></em></p>`
+    ];
+    return textBody.join("");
+}
+
+function getSpManual(sportArray, name, surcharge){}
+
+function getSpDaily(id, date_s, sports){
+    let locDate = date_s.toString().split(" GMT")[0];
+    let textBody = [`<h4><span style="color: #000000;"><em><strong>Dear Student ${id},</strong></em></span></h4>
+                    <p style="text-align: left;"><span style="color: #000000;">&emsp;
+                    Following are the <em><strong>IDs</strong></em> of the good which are due on <strong>${locDate}</strong>:</span>
+                    </p><ol>`];
+    sports.forEach(elem => {
+        textBody.push(`<li style="text-align: left;"><span style="color: #000000;">${elem}</span></li>`);
+    });
+    textBody.push(`</ol><p><em><span style="color: #ff0000;">
+                    Note: Please return the due goods on time or you will have to pay a 100/- surcharge</span></em></p>
+                    <p style="font-size: 90%;"><em><span style="text-decoration: underline;">
+                    *This is a system generated E-mail. No response is required*</span></em></p>`
+    );
+    return textBody.join("");
+}
 
 module.exports = {
     dailyBookNote: getDailyNotification,
     manualBookNote: getManualNotification,
     borrowNote: getBorrowNotification,
     returnNote: getReturnNotification,
-    registerNote: getRegisterNotification
+    registerNote: getRegisterNotification,
+    sportBorrow: getSpBorrow,
+    sportReturn: getSpReturn,
+    sportDaily: getSpDaily,
 }
